b61e89d3871b2f364cfbaa1db72b6af72aaa0b72
Igor Gaidai <gaidai92@gmail.com>
2024-02-03 19:46:31 -0500

    Linear approximation

--- main.py
+++ main.py
@@ -25,10 +25,12 @@ def evaluate_score_stage_1(results, user_input_precision):
     for x_val in domain:
         x_val_floored = int(x_val * (2 ** user_input_precision)) / (2 ** user_input_precision)
         measured_y.append(results_dict[x_val_floored])
+    measured_y = np.array(measured_y)
 
-    # The metric assesses what's the largest distance between the expected value and the measured value, the smallest the better
-    max_distance = np.max(np.abs(expected_y - np.array(measured_y)))
-    return max_distance
+    diff = np.abs(expected_y - measured_y)
+    max_distance = np.max(diff)
+    max_ind = np.argmax(diff)
+    return max_distance, domain[max_ind]
 
 
 def print_depth_width(quantum_program):
@@ -72,16 +74,24 @@ def bits_to_int(bits: ndarray) -> int:
     return sum([2 ** (len(bits) - 1 - ind) for ind, val in enumerate(bits) if val == 1])
 
 
-def transform_const(const: float, num_bits: int) -> tuple[float, int, int]:
+def transform_val(val: float, num_bits_val: int, num_bits_integer: int, num_bits_decimal: int) -> tuple[float, int, int]:
     """
-    Rounds given constant to specified precision and returns representations of the result.
-    :param const: Constant.
-    :param num_bits: Number of bits of precision.
-    :return: 1) Float representation; 2) Int representation; 3) Highest non-zero bit index; 4) Number of bits after first non-zero.
+    Rounds given value between 0 and 1 to specified precision and returns representations of the result.
+    :param val: Value to convert, between 0 and 1.
+    :param num_bits_val: Number of bits in val conversion.
+    :param num_bits_integer: Number of bits in the result.
+    :param num_bits_decimal: Number of bits to use for value conversion.
+    :return: 1) Float representation; 2) Int representation; 3) Number of zeros on the left from the highest 1.
     """
-    bits = real_to_bits(const, num_bits)
+    assert val >= 0
+    assert val < 1
+    assert num_bits_decimal >= num_bits_val
+
+    bits = real_to_bits(val, num_bits_val)
+    bits = np.concatenate((bits, [0] * (num_bits_decimal - num_bits_val)))
     first_one_ind = np.where(bits == 1)[0][0]
-    return bits_to_real(bits), bits_to_int(bits), first_one_ind, len(bits) - first_one_ind
+    zeros_left = num_bits_integer + first_one_ind
+    return bits_to_real(bits), bits_to_int(bits), zeros_left
 
 
 @QFunc
@@ -97,11 +107,12 @@ def prepare_qnum_plain(int_repr: QParam[int], num_digits: QParam[int], out1: Out
 
 
 @QFunc
-def prepare_qnum_append(int_repr: QParam[int], num_zeros: QParam[int], out2: Output[QNum]):
+def prepare_qnum_append(int_repr: QParam[int], num_zeros: QParam[int], num_decimal_bits: QParam[int], out2: Output[QNum]):
     """
     Prepares QNum in a given int state and prepends with specified number of zeros.
     :param int_repr: Integer value to prepare.
-    :param num_zeros: Number of zeros to append.
+    :param num_zeros: Number of zeros to prepend on the left.
+    :param num_decimal_bits: Number of decimal bits for reinterpretation.
     :param out2: Prepared QNum.
     """
     int_arr = QArray('int_arr')
@@ -109,7 +120,7 @@ def prepare_qnum_append(int_repr: QParam[int], num_zeros: QParam[int], out2: Out
     zeros = QArray('zeros')
     allocate(num_zeros, zeros)
     join(int_arr, zeros, out2)
-    reinterpret_num(False, int_arr.len() + zeros.len(), out2)
+    reinterpret_num(False, num_decimal_bits, out2)
 
 
 # @QFunc
@@ -146,12 +157,15 @@ def mult(a: QParam[float], x: QNum, y: QNum):
 
 @QFunc
 def compute_tanh(precision: QParam[int], x: QNum, tanh_x: Output[QNum]):
-    if taylor_coeffs[0][2] == 0:
-        prepare_qnum_plain(taylor_coeffs[0][1], taylor_coeffs[0][3], tanh_x)
-    else:
-        prepare_qnum_append(taylor_coeffs[0][1], taylor_coeffs[0][2], tanh_x)
+    # if taylor_coeffs[0][2] == 0:
+    #     prepare_qnum_plain(taylor_coeffs[0][1], taylor_coeffs[0][3], tanh_x)
+    # else:
+    #     prepare_qnum_append(taylor_coeffs[0][1], taylor_coeffs[0][2], tanh_x)
 
-    # mult(0.786448, x, tanh_x)
+    prepare_qnum_append(taylor_coeffs[0][1], taylor_coeffs[0][2], precision_y_decimal, tanh_x)
+
+    if len(taylor_coeffs) > 1:
+        mult(taylor_coeffs[1][0], x, tanh_x)
     # first_order = QNum('first_order')
     # multiplication(0.786448, x, first_order)
     # inplace_add(first_order, tanh_x)
@@ -161,17 +175,18 @@ def compute_tanh(precision: QParam[int], x: QNum, tanh_x: Output[QNum]):
 def main(x: Output[QNum], y: Output[QNum]):
     allocate_num(precision_x, False, precision_x, x)
     hadamard_transform(x)
-
-    # allocate_num(precision_x, False, precision_x, y)
-    # mult(2, x, y)
-
     compute_tanh(precision_x, x, y)
 
 
 if __name__ == '__main__':
-    precision_x = 3
-    taylor_coeffs = [0.462117, 0.786448]
-    taylor_coeffs = [transform_const(const, precision_x) for const in taylor_coeffs]
+    precision_x = 6
+    precision_y_integer = 1
+    precision_y_decimal = 6
+    precision_consts = 6
+    # taylor_coeffs = [0.462117]
+    taylor_coeffs = [0.068893, 0.786448]
+
+    taylor_coeffs = [transform_val(val, precision_consts, precision_y_integer, precision_y_decimal) for val in taylor_coeffs]
     qmod = create_model(main)
     qmod = set_constraints(qmod, Constraints(optimization_parameter='width'))
     qprog = synthesize(qmod)
@@ -185,4 +200,5 @@ if __name__ == '__main__':
     diffs = np.array([state.state['y'] - np.tanh(state.state['x']) for state in sorted_counts])
     print(f'Max dist at points: {np.max(abs(diffs))}')
 
-    print(f'Max dist overall: {evaluate_score_stage_1(job_result, precision_x)}')
+    eval_result = evaluate_score_stage_1(job_result, precision_x)
+    print(f'Max dist overall: {eval_result[0]} at x = {eval_result[1]}')
